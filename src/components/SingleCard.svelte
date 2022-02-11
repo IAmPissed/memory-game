0b9e610445d411cd738f6897027995b4abfac4fe
xaniar <joker427861@gmail.com>
2022-02-11 15:13:46 +0330

    add single card component

---
+++ src/components/SingleCard.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let card;
+</script>
+
+<div class="card">
+	<div>
+		<img src={card.src} alt="card front" class="front" />
+		<img src="/img/cover.png" alt="card back" class="back" />
+	</div>
+</div>
+
+<style>
+	.card {
+		position: relative;
+	}
+	.card img {
+		width: 100%;
+		display: block;
+		border: 0.2rem solid white;
+		border-radius: 0.6rem;
+	}
+</style>
